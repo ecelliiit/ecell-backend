59805eeeef57782bf31e9efbabd132e03785475c
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2022-08-06 18:58:13 +0530

    changed origins

--- middleware/middleware.auth.go
+++ middleware/middleware.auth.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ecelliiit/ecell-backend/config"
+	"github.com/ecelliiit/ecell-backend/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth(c *gin.Context) {
+	auth := c.Request.Header.Get("Authorization")
+	secret := strings.Split(auth, " ")[1]
+	fmt.Println("secret", secret)
+
+	if secret != config.Cfg.Secret {
+		utils.SendResponse(c, 401, "you cannot access the request", nil)
+		return
+	}
+
+	c.Next()
+}
--- routes/init.go
+++ routes/init.go
@@ -14,7 +14,7 @@ const (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://www.ecelliiit.in , https://web-frontend-opal.vercel.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH")
@@ -35,6 +35,7 @@ func BuildRoutes() *gin.Engine {
 	engine.Use(gin.Logger())
 	engine.Use(gin.Recovery())
 	engine.Use(CORS())
+	// engine.Use(middleware.Auth)
 
 	//intantiate all routes
 	InitSubscriberRoutes(engine)
