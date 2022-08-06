24a95689ff6738e5ff926b39137c99c7f7d6e3bc
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2022-08-06 19:03:30 +0530

    a

--- routes/init.go
+++ routes/init.go
@@ -14,7 +14,7 @@ const (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://www.ecelliiit.in , https://web-frontend-opal.vercel.app")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://www.ecelliiit.in")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH")
